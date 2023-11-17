fd7963e643b846209d04f978d1019374a0b56c5a
CarlosRyan07 <carlosryan.profissional@gmail.com>
2023-11-17 01:02:38 -0300

    ,

--- Sprint_8/DesafioPart2-Ingestão_de_dados_TMDB/ingestaoLambda.py
+++ Sprint_8/DesafioPart2-Ingestão_de_dados_TMDB/ingestaoLambda.py
@@ -1,6 +1,17 @@
-import requests
+import os
 import json
+import requests
 import time
+import boto3
+
+TMDB_API_KEY = os.environ.get('TMDB_API_KEY')
+TMDB_API_TOKEN = os.environ.get('TMDB_API_TOKEN')
+
+S3_BUCKET_NAME = 'ryan-desafio-bucket'
+S3_PREFIX = 'Raw/Local/JSON'
+
+# Inicialização de clientes
+s3 = boto3.client('s3')
 
 def get_movie_data(movie_id, api_key):
     """Obtém informações adicionais sobre um filme usando o ID do filme."""
@@ -14,17 +25,22 @@ def get_movie_data(movie_id, api_key):
         print(f"Falha na requisição de informações adicionais para o filme com ID {movie_id}. Código de status: {filme_response.status_code}")
         return None
 
-def save_movies_to_file(filmes, contador):
-    """Salva os filmes em um arquivo JSON."""
+def save_movies_to_s3(filmes, contador):
+    """Salva os filmes no S3."""
     file_name = f'filmes_arquivo_{contador}.json'
-    with open(file_name, 'w', encoding='utf-8') as json_file:
-        json.dump(filmes, json_file, ensure_ascii=False, indent=4)
-        print(f"Arquivo {file_name} salvo com sucesso!")
+    s3_path = f"{S3_PREFIX}/{file_name}"
+
+    # Convertendo a lista de filmes para uma string JSON
+    filmes_json = json.dumps(filmes, ensure_ascii=False, indent=4)
+
+    # Upload direto para o S3
+    s3.put_object(Body=filmes_json, Bucket=S3_BUCKET_NAME, Key=s3_path)
+
+    print(f"Arquivo {file_name} enviado com sucesso para o S3.")
 
 def discover_movies(api_key, params):
     """Descobre e coleta informações sobre filmes."""
     url = "https://api.themoviedb.org/3/discover/movie"
-    base_url = "https://api.themoviedb.org/3/movie"
     headers = {"accept": "application/json"}
 
     response = requests.get(url, headers=headers, params=params)
@@ -52,7 +68,7 @@ def discover_movies(api_key, params):
 
                 if len(filmes) >= 100 or page == total_pages:
                     contador += 1
-                    save_movies_to_file(filmes, contador)
+                    save_movies_to_s3(filmes, contador)
                     filmes = []
 
             else:
@@ -60,19 +76,21 @@ def discover_movies(api_key, params):
     else:
         print(f"Erro na requisição: {response.status_code}")
 
-# Configurações iniciais
-api_key = "2866514b7137f96a2f188f3f14e80b37"
-params = {
-    "include_adult": False,
-    "include_video": False,
-    "language": "pt-BR",
-    "page": 1,
-    "primary_release_date.gte": "2018-01-01",
-    "sort_by": "popularity.desc",
-    "vote_count.gte": 10,
-    "with_genres": 80,
-    "api_key": api_key
-}
+def lambda_handler(event, context):
+    """Função de manipulador principal."""
+    # Configurações iniciais
+    api_key = TMDB_API_KEY
+    params = {
+        "include_adult": False,
+        "include_video": False,
+        "language": "pt-BR",
+        "page": 1,
+        "primary_release_date.gte": "2018-01-01",
+        "sort_by": "popularity.desc",
+        "vote_count.gte": 10,
+        "with_genres": 80,
+        "api_key": api_key
+    }
 
-# Executa a descoberta de filmes
-discover_movies(api_key, params)
+    # Executa a descoberta de filmes e salva no S3
+    discover_movies(api_key, params)
